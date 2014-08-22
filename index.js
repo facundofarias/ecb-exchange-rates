ebab64a75c8724f1da60a3019f611a314d425594
Facundo Farias <facundo.farias@gmail.com>
2014-08-22 18:23:59 +0200

    Loaded XML. Trying to get the currencies value.

--- index.js
+++ index.js
@@ -7,16 +7,44 @@ module.exports = {
       url: "http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml"
     },
 
-    requestCallback: function(error, response, body) {
-
-      if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
-      }
-
+    removeNamespaces: function(obj){
+        var r={};
+        for(var k in obj){
+            r[k.replace(/:/g,'$')] = (typeof obj[k]==='object') ? this.removeNamespaces(obj[k]) : obj[k];
+        }
+        return r;
     },
 
+    parseXML: function(xml) {
+      var that = this;
+
+      var parser = new xml2js.Parser();
+      parser.parseString(xml, function(err,result){
+        var currencies = result['gesmes:Envelope'].Cube[0].Cube[0].Cube;
+        var timestamp = result['gesmes:Envelope'].Cube[0].Cube[0].time;
+        that.createCurrenciesMap(currencies);
+      });
+    },
+
+    createCurrenciesMap: function(currencies) {
+      console.log(JSON.stringify(this.removeNamespaces(currencies)));
+      var curatedCurrencies = new Array();
+        currencies.forEach(function(item) {
+          //curatedCurrencies.push({ item.$.currency, item.$.rate });
+       });
+       console.log(JSON.stringify(curatedCurrencies));
+    },
+
+
     getCurrencies: function() {
-      request(this.settings.url, this.requestCallback);
+      var that = this;
+      request(this.settings.url, function(error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+          that.parseXML(body);
+        }
+
+      });
     }
 
 };
