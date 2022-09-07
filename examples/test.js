f399c7b3f7dc25f2630bf0693860d44d4821a394
Pablo Leon <pabloleonpsico@gmail.com>
2022-09-07 10:57:08 +0200

    Remove commented code
    I was testing the library and found some commented code that could be removed

--- examples/test.js
+++ examples/test.js
@@ -1,36 +1,5 @@
 var currencyConverter = require('../currencyConverter.js');
 
-// currencyConverter.getAllCurrencies(function(data){
-//   console.log(JSON.stringify(data));
-// });
-
-// currencyConverter.getBaseCurrency(function(data){
-//   console.log(JSON.stringify(data));
-// });
-
-// var settings = {};
-// settings.fromCurrency = "GBP";
-// settings.toCurrency = "USD";
-// settings.amount = 90;
-// settings.accuracy = 5;
-//
-// currencyConverter.convert(settings , function(data){
-//   console.log(JSON.stringify(data));
-// });
-
-// var settings = {};
-// settings.fromCurrency = "GBP";
-// settings.toCurrency = "USD";
-// settings.accuracy = 5;
-//
-// currencyConverter.getExchangeRate(settings , function(data){
-//   console.log(JSON.stringify(data));
-// });
-
-// currencyConverter.getCurrenciesMetadata(function(data){
-//   console.log(JSON.stringify(data));
-// });
-
 var settings = {};
 settings.currency = "EUR";
 currencyConverter.getCurrencyMetadata(settings, function(data){
